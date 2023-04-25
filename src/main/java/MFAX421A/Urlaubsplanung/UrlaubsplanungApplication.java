959c007799c27db81fc245aed2b90da2dee7ca24
LolXpro <76632611+LolXpro@users.noreply.github.com>
2023-04-25 14:19:34 +0200

    Initial Commit

---
+++ src/main/java/MFAX421A/Urlaubsplanung/UrlaubsplanungApplication.java
@@ -0,0 +1,13 @@
+package MFAX421A.Urlaubsplanung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlaubsplanungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlaubsplanungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MFAX421A/Urlaubsplanung/UrlaubsplanungApplicationTests.java
@@ -0,0 +1,13 @@
+package MFAX421A.Urlaubsplanung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlaubsplanungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
